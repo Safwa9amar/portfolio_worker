c3f994eea25199a0a087ee556739479ba37b58c7
Safwa9amar <hassanih97@gmail.com>
2022-09-20 23:00:37 +0100

    all

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from app import db,ma
+from app import db, ma
+import requests
+
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,10 +10,13 @@ class Blog(db.Model):
     resume = db.Column(db.String, nullable=False)
     img_url = db.Column(db.String, nullable=False, default='default.jpeg')
     en_notion_id = db.Column(db.String, nullable=False)
+    en_notion = db.Column(db.String)
     ar_notion_id = db.Column(db.String, nullable=False,
                              default='Not available')
+    ar_notion = db.Column(db.String)
     ar_darija_notion_id = db.Column(
         db.String, nullable=False, default='Not available')
+    darija_notion = db.Column(db.String)
     date_posted = db.Column(db.Date, nullable=False,
                             default=datetime.utcnow)
 
@@ -22,3 +27,12 @@ class Blog(db.Model):
 class BlogsSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Blog
+
+
+def FetchNotionApi(notio_id):
+    if notio_id:
+        url_api = f'https://notion-api.splitbee.io/v1/page/{notio_id}'
+        req = requests.get(url_api)
+        return req.content
+    else:
+        return 'No data !'
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,9 @@
-from email import header
 from flask import render_template, url_for, flash, redirect
 from app.forms import RegForm
-from app.models import Blog, BlogsSchema
+from app.models import Blog, BlogsSchema, FetchNotionApi
 from app import app, db
 from flask import jsonify
-
+import json
 data = [
     {
         'header': 'How To Use CSS Layers',
@@ -33,15 +32,18 @@ def about():
     posts = Blog.query.all()
     post_schema = BlogsSchema(many=True)
     output = post_schema.dump(posts)
-    return jsonify({"post":output})
+    return jsonify({"post": output})
     # return render_template('blogs_worker.html', posts = posts)
-    
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     form = RegForm()
     if form.validate_on_submit():
+        en_notion = FetchNotionApi(form.en_notion_id.data)
+        ar_notion = FetchNotionApi(form.ar_notion_id.data)
+        darija_notion = FetchNotionApi(form.ar_darija_notion_id.data)
+        print(json.loads(en_notion))
         post = Blog(
             header=form.header.data,
             tag=form.tag.data,
@@ -51,6 +53,9 @@ def add():
             ar_notion_id=form.ar_notion_id.data,
             ar_darija_notion_id=form.ar_darija_notion_id.data,
             date_posted=form.date.data,
+            en_notion = en_notion ,
+            ar_notion = ar_notion ,
+            darija_notion = darija_notion 
         )
         db.session.add(post)
         db.session.commit()
